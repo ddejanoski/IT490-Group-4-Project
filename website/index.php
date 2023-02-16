29fdeeb39bb6aa1bd74295a86ccec3cdd47e6de5
nicole <nc442@njit.edu>
2023-02-16 17:59:47 -0500

    rabbitmq files

---
+++ website/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Professor in Space </title>
+	</head>
+
+	<body>
+		<h2> Welcome to Professor in Space! </h2>
+
+		<p> To Be Continued... </p>
+
+		<?php
+			echo "<p> Hello World! </p>";
+		?>
+
+		<p> helllooooo </p>
+
+	</body>
+</html>
