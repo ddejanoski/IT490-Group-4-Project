46fa847367476e578a084183c267e288c8bb4bd2
nicole <nc442@njit.edu>
2023-03-13 15:39:44 -0400

    navbar

---
+++ RabbitMQClient.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$client = new rabbitMQClient("testtRabbiitMQ.ini","testServer");
+//echo "ppdClient BEGIN".PHP_EOL;
+
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+?>
---
+++ website/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Professor in Space </title>
+	</head>
+
+	<body>
+		<h2> Welcome to Professor in Space! </h2>
+	</body>
+</html>
