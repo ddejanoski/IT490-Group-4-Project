056bf53656a10ef59fbd170ee08153c80a0e553b
nicole <nc442@njit.edu>
2023-04-18 19:40:40 -0400

    housekeeping

---
+++ RabbitMQClientGame.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+//require_once('receiveRegister.php');
+
+$client = new rabbitMQClient("registerRMQ.ini","testServer");
+
+if(isset($_GET['register'])){
+	$request = array();
+	$request['type'] = "register";
+	$request['email'] = $_GET["email"];
+	$request['username'] = $_GET["username"];
+	$request['password'] = $_GET["password"];
+
+	$request['message'] = $msg;
+
+	$response = $client->send_request($request);
+
+	print_r($response);
+	
+	if ($response==0){
+		header('location: register.php');
+	}
+	else{ 
+		$_SESSION['email'] = $request['email'];
+      	$_SESSION['success'] = "You are now registered";
+		$_SESSION['json'] = json_decode($response);
+		header('location: game.php');	
+	}
+}
+
+?>
